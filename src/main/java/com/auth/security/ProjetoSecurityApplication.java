10754f85f45961335c8191d25e648837abf03228
Júlio César Fragoso de Lima <103080100+chakaartt@users.noreply.github.com>
2022-11-01 22:38:33 -0300

    first commit

---
+++ src/main/java/com/auth/security/ProjetoSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.auth.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSecurityApplication.class, args);
+	}
+
+}
